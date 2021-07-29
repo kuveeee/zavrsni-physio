de189389dc813b0ab390e7396c67ca516879f91a
Domagoj <domagojkuvezdic@gmail.com>
2021-07-29 12:23:01 +0200

    Changes to App.js and Header.js
    Adding contend and styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Header from './Header.js';
 
 //importanje asseta
 import nova_rezervacija from './assets/nova_rezervacija_slika.png';
-import trajanje_usluge from './assets/trajanje_usluge.png'
+import trajanje_usluge from './assets/trajanje_usluge.png';
+import cijena_usluge from './assets/cijena_usluge.png';
 
 function App() {
     return (
@@ -11,24 +12,35 @@ function App() {
         <Header />
         <div class="app_body">
             <h1>Početna ploča</h1>
-            <div class = "calendar_widget" >
-                Calender
+            <br></br>
+            <div class = "kalendar" >
+                <p>Kalendar</p>
             </div>
             <div class = "nova_rezervacija" >
-                Nova rezervacija 
+                <p>Nova rezervacija</p>
                 <img class="nova_rezervacija_slika" src={nova_rezervacija} alt="Nova rezervacija" />
             </div>
             <div class = "klijenti" >
-                Klijenti 
+                <p>Klijenti</p>
+                250
             </div>
             <div class = "financije" >
-                Financije 
+                <p>Financije</p> 
             </div>
             <div class = "usluge" >
-                Usluge
+                <p>Usluge</p>
                 <div class="tablica">
-                    Naziv usluge
-                    <img class="trajanje usluge" src={trajanje_usluge} alt="trajanje_usluge"></img>
+                    <ul>
+                        <ol>
+                            <h3>Naziv usluge</h3>
+                        </ol>
+                        <ol>
+                            <img class="trajanje usluge" src={trajanje_usluge} alt="trajanje_usluge"></img>
+                        </ol>
+                        <ol>
+                        <img class="cijena usluge" src={cijena_usluge} alt="cijena_usluge"></img>
+                        </ol>
+                    </ul>
                 </div>
             </div>
         </div>
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import './Header.css';
 
 //importanje asseta
+import logo from './assets/logo.png';
 import slika_profila from './assets/slika_profila.png';
 
 function Header() {
@@ -8,12 +9,15 @@ function Header() {
     <>
         <div class="header_body">
             <div class="logo">
-                <img src='./assets/logo.png' alt="Logo"></img>
+                <img src={logo} alt="Logo"></img>
             </div>
-            <iframe title="Datum i vrijeme" src="https://free.timeanddate.com/clock/i7wvbhef/n4461/tlhr30/fn17/fs30/tt0/tw0/tm1/ts1" frameborder="0" width="312" height="39"></iframe>
-            <h1>HEADER HEADER IS HERE</h1>
-            Roberto
-            <img class="slika_profila" src={slika_profila} alt="Slika profila" />
+            <div calss="datum_vrijeme">
+                <iframe title="Datum i vrijeme" src="https://free.timeanddate.com/clock/i7wvbhef/n4461/tlhr30/fn17/fs30/tt0/tw0/tm1/ts1" frameborder="0" width="312" height="39"></iframe>
+            </div>
+            <div class="korisnik">
+                <p>Roberto</p>
+                <img class="slika_profila" src={slika_profila} alt="Slika profila" />
+            </div>
         </div>
     </>
     )
